29b2f532d3c37af6726a984a6da26fef2246e629
Vladislav <vlad1988.1@gmail.com>
2022-07-07 18:15:24 +0300

    Добавьте файлы проекта.

---
+++ Lessons_12_1_ConceptOfAnAlgorithm/User.cs
@@ -0,0 +1,8 @@
+﻿namespace Lessons_12_1_ConceptOfAnAlgorithm;
+
+internal class User
+{
+	public string Login { get; set; }
+	public string Name { get; set; }
+	public bool IsPremium { get; set; }
+}
